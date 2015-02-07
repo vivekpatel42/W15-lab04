f7893eb16be1b9cf5bf901cec4709caf54f6e7c9
Vivek Patel <vivek_patel@umail.ucsb.edu>
2015-02-06 16:49:39 -0800

    Added updated version of SimpleGUI1 to my src directory - done with Step 5

---
+++ src/edu/ucsb/cs56/w15/drawings/vivek_patel/SimpleGui1.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs56.w15.drawings.vivek_patel;
+import javax.swing.*;
+import java.awt.*;
+
+/** SimpleGui1 comes from Head First Java 2nd Edition p. 355.
+      It illustrates a simple GUI with a Button that doesn't do anything yet.
+
+     @author Head First Java, 2nd Edition p. 355
+     @author P. Conrad (who only typed it in and added the Javadoc comments)
+     @author Vivek Patel 
+     @version CS56, Winter 2015, UCSB
+*/
+
+public class SimpleGui1 {
+
+    /** main method to fire up a JFrame on the screen  
+          @param args not used
+    */
+
+    public static void main (String[] args) {
+	JFrame frame = new JFrame() ;
+	JButton button = new JButton("oh yes pls");
+	Color myColor = new Color(0, 51, 51);
+	button.setBackground(myColor);
+	button.setOpaque(true);
+	frame. setDefaultCloseOperation(JFrame. EXIT_ON_CLOSE) ;
+	frame. getContentPane() . add(button) ;
+	frame. setSize(300,300) ;
+	frame. setVisible(true) ;
+    }
+}
